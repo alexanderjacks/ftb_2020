9971c1f1602687a3ae2affeb5d814d5dc4338f13
Alexander Jacks <alexanderthejacks@gmail.com>
2020-03-11 10:21:03 -0700

    🎉 1st 1

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'Fight The Ban', // Site title.
+  siteTitleAlt: 'FTB2020', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Fight The Ban on Trans Persons Serving', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
   disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
+  userName: 'minorityvets',
+  userTwitter: 'minorityvets',
+  userLocation: 'Seattle, USA',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#ffffff', // Used for setting manifest background color.
+  copyright: 'MIT, MXX', // Copyright string for the footer of the website and RSS feed.
+  themeColor: 'navy', // Used for setting manifest and progress theme colors.
+  backgroundColor: 'snow', // Used for setting manifest background color.
 }
